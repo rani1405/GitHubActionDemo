501bf9a050245e965373e2bc5abbdb084039a0c6
rani1405 <jdpatel029@gmail.com>
2025-05-04 18:02:40 -0400

    test github action failure

--- src/main/java/com/example/postgreSQLDemo/demo/controller/StudentController.java
+++ src/main/java/com/example/postgreSQLDemo/demo/controller/StudentController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/student")
 public class StudentController {
 
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to GitHubAction !";
+    }
+
     @Autowired
     private StudentService studentService;
 
